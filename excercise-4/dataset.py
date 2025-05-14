4339589080d107b197d13a63229b13b021ef4730
Mateusz Kubiak <matkubiak888@gmail.com>
2025-05-14 20:52:16 +0200

    augmentation MK

--- excercise-4/dataset.py
+++ excercise-4/dataset.py
@@ -3,8 +3,16 @@ import torchvision.datasets as datasets
 import torchvision.transforms as transforms
 import pytorch_lightning as pl
 
+mnist_transforms = {
+    'default': transforms.ToTensor(),
+    'MK_random_rotate': transforms.Compose([
+            transforms.RandomRotation(degrees=5),
+            transforms.ToTensor()
+        ])
+}
+
 class MnistDataModule(pl.LightningDataModule):
-    def __init__(self, data_dir, batch_size, num_workers):
+    def __init__(self, data_dir, batch_size, num_workers, transform='default', limit_samples=0):
         super().__init__()
         self.train_loader = None
         self.val_dataset = None
@@ -12,15 +20,16 @@ class MnistDataModule(pl.LightningDataModule):
         self.data_dir = data_dir
         self.batch_size = batch_size
         self.num_workers = num_workers
+        self.transform = mnist_transforms[transform]
 
     def prepare_data(self):
         datasets.MNIST(root=self.data_dir, train=True, download=True)
         datasets.MNIST(root=self.data_dir, train=False, download=True)
 
     def setup(self, stage):
-        entire_dataset = datasets.MNIST(root=self.data_dir, train=True,  transform=transforms.ToTensor(),  download=False)
+        entire_dataset = datasets.MNIST(root=self.data_dir, train=True,  transform=self.transform,  download=False)
         self.train_dataset, self.val_dataset = random_split(entire_dataset, [50000, 10000])
-        self.train_loader = datasets.MNIST(root=self.data_dir, train=True,  transform=transforms.ToTensor(),  download=False)
+        self.train_loader = datasets.MNIST(root=self.data_dir, train=True,  transform=self.transform,  download=False)
 
     def train_dataloader(self):
         return DataLoader(self.train_loader, batch_size=self.batch_size, num_workers=self.num_workers, shuffle=True)
@@ -31,11 +40,20 @@ class MnistDataModule(pl.LightningDataModule):
     def test_dataloader(self):
         return DataLoader(self.val_dataset, batch_size=self.batch_size, num_workers=self.num_workers, shuffle=False)
 
-
-
+imagenette_transforms = {
+    'default': transforms.Compose([
+            transforms.Resize((160, 160)),  # Resize to 160x160
+            transforms.ToTensor(),
+        ]),
+    'MK_random_crop': transforms.Compose([
+            transforms.Resize((192, 192)),
+            transforms.RandomCrop((160, 160)),
+            transforms.ToTensor()
+        ])
+}
 
 class ImageNetteDataModule(pl.LightningDataModule):
-    def __init__(self, data_dir, batch_size, num_workers):
+    def __init__(self, data_dir, batch_size, num_workers, transform='default'):
         super().__init__()
         self.train_loader = None
         self.val_dataset = None
@@ -43,33 +61,20 @@ class ImageNetteDataModule(pl.LightningDataModule):
         self.data_dir = data_dir
         self.batch_size = batch_size
         self.num_workers = num_workers
-
-        # Define the transformation pipeline
-        self.transform = transforms.Compose([
-            transforms.Resize((160, 160)),  # Resize to 160x160
-            transforms.ToTensor(),
-        ])
+        self.transform = imagenette_transforms[transform]
 
     def prepare_data(self):
         datasets.Imagenette(root=self.data_dir, size="160px", download=True)
 
     def setup(self, stage=None):
-        # Apply the transformation to the dataset
         entire_dataset = datasets.Imagenette(root=self.data_dir, size="160px", transform=self.transform, download=False)
-
-        # Randomly split the entire dataset
-        self.train_dataset, self.val_dataset = random_split(
-            entire_dataset, [8000, 1469]  # Adjust split if necessary
-        )
+        self.train_dataset, self.val_dataset = random_split(entire_dataset, [8000, 1469])
 
     def train_dataloader(self):
-        # Return the train DataLoader
         return DataLoader(self.train_dataset, batch_size=self.batch_size, num_workers=self.num_workers, shuffle=True)
 
     def val_dataloader(self):
-        # Return the validation DataLoader
         return DataLoader(self.val_dataset, batch_size=self.batch_size, num_workers=self.num_workers, shuffle=False)
 
     def test_dataloader(self):
-        # Return the test DataLoader (same as validation here)
         return DataLoader(self.val_dataset, batch_size=self.batch_size, num_workers=self.num_workers, shuffle=False)
