cafad4cfefc819aab35824058df5f0df5e8eb8a6
Mateusz Kubiak <matkubiak888@gmail.com>
2025-06-09 23:24:59 +0200

    config update

--- excercise-3/config.py
+++ excercise-3/config.py
@@ -1,11 +1,13 @@
+import os
+
 # Hyper Parameters
 INPUT_SIZE = 784
 NUM_CLASSES = 10
 LEARNING_RATE = 0.001
 BATCH_SIZE = 64
-HIDDEN_LAYERS = 50
-EPOCHS = 1
+HIDDEN_LAYERS = 150
+EPOCHS = 60
 
 # Data Set
-NUM_WORKERS = 0
+NUM_WORKERS = os.cpu_count() - 1
 DATA_DIR = "./dataset"
