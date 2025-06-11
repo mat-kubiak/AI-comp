c0c433b85983105a46fea16bfa8d41bf1362645e
Mateusz Kubiak <matkubiak888@gmail.com>
2025-06-11 20:01:58 +0200

    lime with segmentation + better viz

--- excercise-5/mlp_mnist_high_feature.py
+++ excercise-5/mlp_mnist_high_feature.py
@@ -13,6 +13,8 @@ from captum._utils.models.linear_model import SkLearnLinearRegression, SkLearnLa
 
 import matplotlib.pyplot as plt
 
+from skimage.segmentation import slic
+
 def choose_dataset():
     print("Choose a feature extracton method:")
     print("1 - flatten")
@@ -40,6 +42,59 @@ def plot_heatmap(data, title=''):
     if title != '':
         ax.set_title(title)
     fig.colorbar(im, ax=ax)
+    plt.show()
+
+def shuffle_by_value(data):
+    unique_data = np.unique(data)
+    shuffled_labels = np.random.permutation(unique_data)
+
+    label_map = dict(zip(unique_data, shuffled_labels))
+
+    return np.vectorize(label_map.get)(data)
+
+def plot_2_part_heatmap(original_data, attr_data, title=''):
+    max_val = np.abs(attr_data).max()
+
+    fig, axes = plt.subplots(1, 2, figsize=(8, 4))
+
+    # Original image
+    axes[0].imshow(original_data, cmap='gray')
+    axes[0].set_title('Original Image')
+
+    # Attribution heatmap
+    im = axes[1].imshow(attr_data, vmin=-max_val, vmax=max_val, cmap='bwr')
+    axes[1].set_title('Attributions')
+    fig.colorbar(im, ax=axes[1])
+
+    if title:
+        fig.suptitle(title)
+
+    plt.tight_layout()
+    plt.show()
+
+def plot_3_part_heatmap(original_data, segment_data, attr_data, title=''):
+    max_val = np.abs(attr_data).max()
+
+    fig, axes = plt.subplots(1, 3, figsize=(12, 4))
+
+    # Original image
+    axes[0].imshow(original_data, cmap='gray')
+    axes[0].set_title('Original Image')
+
+    # Segmentation map
+    shuffled_segments = shuffle_by_value(segment_data)
+    axes[1].imshow(shuffled_segments, cmap='nipy_spectral')
+    axes[1].set_title('Segmentation')
+
+    # Attribution heatmap
+    im = axes[2].imshow(attr_data, vmin=-max_val, vmax=max_val, cmap='bwr')
+    axes[2].set_title('Attributions')
+    fig.colorbar(im, ax=axes[2])
+
+    if title:
+        fig.suptitle(title)
+
+    plt.tight_layout()
     plt.show()
 
 def main():
@@ -88,22 +143,36 @@ def main():
 
     attributions = np.reshape(attributions.cpu(), (5, 28, 28))
     for i in range(len(attributions)):
-        plot_heatmap(attributions[i], f'Integrated Gradients\ny_true: {labels[i]}, y_pred: {preds[i]}')
+        plot_2_part_heatmap(np.reshape(samples[i].cpu(), (28, 28)), attributions[i], f'Integrated Gradients\ny_true: {labels[i]}, y_pred: {preds[i]}')
 
     # LIME
-    # print('\n### LIME ##############################\n')
-    # lime = Lime(
-    #     forward_func=model,
-    #     interpretable_model=SkLearnLinearRegression(),  # Surrogate model
-    #     similarity_func=None  # Use default (exponential kernel)
-    # )
+    print('\n### LIME ##############################\n')
+    lime = Lime(
+        forward_func=model,
+        interpretable_model=SkLearnLinearRegression(),  # Surrogate model
+        similarity_func=None  # Use default (exponential kernel)
+    )
 
-    # for i in range(samples.shape[0]):
-    #     sample = samples[i].unsqueeze(0)  # shape [1, features]
-    #     label = labels[i].item()
-    #     attributions = lime.attribute(sample, target=label, n_samples=2000)
+    for i in range(samples.shape[0]):
+        sample = samples[i].unsqueeze(0)  # shape [1, features]
+        label = labels[i].item()
 
-    #     plot_heatmap(np.reshape(attributions.cpu(), (28, 28)), f'LIME\ny_true: {labels[i]}, y_pred: {preds[i]}')
+        sample_np = sample.squeeze().cpu().numpy().reshape(28, 28)
+        sample_rgb = np.stack([sample_np]*3, axis=-1)
+
+        segments = slic(sample_rgb, n_segments=100, compactness=1)
+        segments = np.unique(segments, return_inverse=True)[1].reshape(segments.shape) # make segments start with 0
+
+        feature_mask = torch.tensor(segments, dtype=torch.long).view(-1).unsqueeze(0).to(device)
+
+        attributions = lime.attribute(sample, target=label, n_samples=1500, feature_mask=feature_mask)
+        attr = attributions.squeeze().cpu().numpy()
+
+        heatmap = np.zeros_like(segments, dtype=np.float32)
+        for seg_val in np.unique(segments):
+            heatmap[segments == seg_val] = attr[seg_val]
+
+        plot_3_part_heatmap(sample_rgb, segments, heatmap, title=f'LIME\ny_true: {labels[i]}, y_pred: {preds[i]}')
 
 if __name__ == '__main__':
     main()
